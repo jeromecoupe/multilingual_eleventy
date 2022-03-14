f240db3df22b836e8ae253c5732faf225a4324e2
Jérôme Coupé <jercoupe@gmail.com>
2022-03-14 12:55:48 +0100

    Group date filters

--- .eleventy.js
+++ .eleventy.js
@@ -4,21 +4,19 @@ module.exports = function(eleventyConfig) {
 
   // EN
   eleventyConfig.addCollection("events_en", require("./src/_11ty/collections/events_en.js"));
-  
   eleventyConfig.addCollection("posts_en", require("./src/_11ty/collections/posts_en.js"));
 
   // FR
   eleventyConfig.addCollection("events_fr", require("./src/_11ty/collections/events_fr.js"));
-  
   eleventyConfig.addCollection("posts_fr", require("./src/_11ty/collections/posts_fr.js"));
 
   // FILTERS
   // -------------------------------
-  eleventyConfig.addFilter("dateFeed", require("./src/_11ty/filters/dateFeed.js"));
-  eleventyConfig.addFilter("dateFormat", require("./src/_11ty/filters/dateFormat.js"));
-  eleventyConfig.addFilter("dateFull", require("./src/_11ty/filters/dateFull.js"));
-  eleventyConfig.addFilter("dateIso", require("./src/_11ty/filters/dateIso.js"));
-  eleventyConfig.addFilter("dateYear", require("./src/_11ty/filters/dateYear.js"));
+  eleventyConfig.addFilter("dateFeed", require("./src/_11ty/filters/date.js").dateFeed);
+  eleventyConfig.addFilter("dateFormat", require("./src/_11ty/filters/date.js").dateFormat);
+  eleventyConfig.addFilter("dateFull", require("./src/_11ty/filters/date.js").dateFull);
+  eleventyConfig.addFilter("dateIso", require("./src/_11ty/filters/date.js").dateIso);
+  eleventyConfig.addFilter("dateYear", require("./src/_11ty/filters/date.js").dateYear);
 
   // COPY
   // -------------------------------
